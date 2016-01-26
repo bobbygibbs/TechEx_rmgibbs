a26eb67f46344df6799ff72c353f83dfea474cef
Ubuntu <ubuntu@ip-172-31-24-58.us-west-2.compute.internal>
2016-01-26 23:19:19 +0000

    Initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = 'b64875e401286955ba44f50123dba0f018d04b4c6e05d5fb33700c8814dad683edaa09da8abc189bdcb305692f5613e20131226068acedf87b35689b299ed369'
